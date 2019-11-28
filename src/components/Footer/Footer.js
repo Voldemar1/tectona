b1c3156278e19fdc07a69c5f3f90afa49fc8dc83
Voldemar <voldemar136@gmail.com>
2019-11-28 21:03:17 +0300

    add dynamic year

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types'
 import './Footer.scss'
 
 const Footer = props => {
+  const year =
+    new Date().getFullYear() === 2019
+      ? 2019
+      : `2019 - ${new Date().getFullYear()}`
   return (
     <div className='footer'>
       <a href="mailto:tectonaby@gmail.com">tectonaby@gmail.com</a>
       <a href="tel:+375293361533">+375-29-336-15-33</a>
-      <h6>© {new Date().getFullYear()} Тектона Консалт</h6>
+      <h6>© {year} Тектона Консалт</h6>
     </div>
   )
 }
