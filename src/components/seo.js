bf2371654d3426865b509bbaf8bf5167bb15d5b8
Valdemar <voldemar136@gmail.com>
2019-12-02 20:16:00 +0300

    fix title

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,7 +32,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={"Тектона"}
+      title="Тектона"
       // title={title}
       // titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
