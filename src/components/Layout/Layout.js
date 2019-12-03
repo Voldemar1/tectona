533ee1a66a9ffc1e51980872989193effb07acb8
Valdemar <voldemar136@gmail.com>
2019-12-03 21:30:35 +0300

    no message

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "../header/Header"
+import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 
 import "./Layout.scss"
