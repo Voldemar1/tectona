5251341b377aeac3f143a55459d6e29b181e799e
Valdemar <voldemar136@gmail.com>
2019-12-03 20:37:41 +0300

    refactoring

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,11 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header/header"
-import Footer from './Footer/Footer'
+import Header from "../Header/Header"
+import Footer from '../Footer/Footer'
 
-import "./layout.css"
+import "./Layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,11 +19,11 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <div className="layout">
       <Header />
         <main>{children}</main>
       <Footer />
-  </>
+    </div>  
   )
 }
 
