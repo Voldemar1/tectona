7c85708f6f719b375627661a153b199e4fcab7f9
Valdemar <voldemar136@gmail.com>
2019-12-11 14:02:20 +0300

    fix build error

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from '../Header/Header'
+import Header from '../header/header'
 import Footer from '../Footer/Footer'
 
 import "./Layout.scss"
