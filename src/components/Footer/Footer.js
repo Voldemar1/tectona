b823ce73218b649adf910109d5cdc6551d96822f
Valdemar <voldemar136@gmail.com>
2019-12-12 22:43:52 +0300

    delete contacts from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,9 +9,7 @@ const Footer = props => {
       : `2019 - ${new Date().getFullYear()}`
   return (
     <div className='footer'>
-      <a href="mailto:tectonaby@gmail.com">tectonaby@gmail.com</a>
-      <a href="tel:+375293361533">+375-29-336-15-33</a>
-      <h6>© {year} Тектона Консалт</h6>
+      <h5>© {year} Тектона Консалт</h5>
     </div>
   )
 }
