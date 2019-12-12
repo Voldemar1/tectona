834caeca03ddd0458b8624a59ce99895bee103e8
Valdemar <voldemar136@gmail.com>
2019-12-12 22:44:42 +0300

    new design for header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,6 +5,33 @@ import "./header.scss"
 const Header = props => (
   <div className="header">
     <img src={require("./images/logo.png")} alt="" />
+    <div className="contacts">
+      <div className="phones-block">
+        <div>
+        <img className="phones-svg" src={require("./images/telegram.svg")} alt="" />
+        <img className="phones-svg" src={require("./images/whatsapp.svg")} alt="" />
+        <img className="phones-svg" src={require("./images/viber.svg")} alt="" />
+        <img className="phones-svg" src={require("./images/phone.svg")} alt="" />
+        </div>
+        <div className="phones">
+        <a href="tel:+375293361533">+375-29-336-15-33</a>
+        <a href="tel:+375293659445">+375-29-365-94-45</a>
+        </div>
+      </div>
+    
+    <a className="fax" href="fax:+375177452349">
+      <img src={require("./images/fax.svg")} alt="" />
+      +375-1774-52-3-49
+    </a>   
+    <a className="mail" href="mailto:tectonaby@gmail.com">
+      <img src={require("./images/gmail.svg")} alt="" />
+      tectonaby@gmail.com
+    </a>
+    <a className="skype" href="skype:valentin041">
+      <img src={require("./images/skype.svg")} alt="" />
+      valentin041
+    </a>
+    </div>
   </div>
 )
 
