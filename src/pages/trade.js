d4389cb97612b7dfbb534a48aeb53deeefcedab1
Valdemar <voldemar136@gmail.com>
2019-12-14 19:17:22 +0300

    add trade page

---
+++ src/pages/trade.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Layout from '../components/Layout/Layout'
+
+const trade = props => {
+    return (
+        <Layout>
+            
+        </Layout>
+    )
+}
+
+trade.propTypes = {
+
+}
+
+export default trade
