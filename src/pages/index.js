243f6e18f78ecf2014e4683882fd4343bfacbc8e
Valdemar <voldemar136@gmail.com>
2019-12-14 19:16:46 +0300

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/Layout/Layout"
-import SEO from "../components/seo"
-import ConsultingServices from "../components/ConsultingServices/ConsultingServices"
+import Layout from "../components/Layout/Layout";
+import SEO from "../components/seo";
+import Menu from "../components/Menu/Menu";
+
+import "./index.scss";
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <ConsultingServices />
+  <div className="index">
+    <Layout >
+      <SEO title="Home" />
+      <h2>Группа компаний "Тектона"</h2>
+      <Menu />
   </Layout>
+  </div>
 )
 
 export default IndexPage
