33610ea7690280899638e1403363aab21ba07ba6
Valdemar <voldemar136@gmail.com>
2019-12-14 19:20:06 +0300

    init it page

---
+++ src/pages/it.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const it = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+it.propTypes = {
+
+}
+
+export default it
