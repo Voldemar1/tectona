e9366fdcba8a6fc9da1b925d6c609c2e4ac05c09
Valdemar <voldemar136@gmail.com>
2020-06-16 15:26:31 +0300

    create menu for fish

---
+++ src/components/MenuFish/MenuFish.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import "./MenuFish.scss"
+
+function MenuFish() {
+  return (
+    <div className="menufish">
+      <Link to="/gorbusha">Горбуша</Link>
+      <Link to="/pollock">Минтай</Link>
+      <Link to="/herring">Сельдь</Link>
+      <Link to="/anchovy">Хамса</Link>
+      <Link to="/mackerel">Скумбрия</Link>
+      <Link to="/sprat">Килька</Link>
+      <Link to="/capelin">Мойва</Link>
+      <Link to="/cod">Треска</Link>
+      <Link to="/whiting">Путассу</Link>
+      <Link to="/squid">Кальмар</Link>
+    </div>
+  )
+}
+
+export default MenuFish
