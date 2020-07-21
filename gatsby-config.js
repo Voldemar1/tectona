6151f8950abc276f738400b1a6f9b00f16af3b86
Valdemar <voldemar136@gmail.com>
2020-07-21 13:48:17 +0300

    add chat

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,15 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-crisp-chat",
+      options: {
+        websiteId: "b0b61637-409e-4012-b2f4-048689243f9c",
+        enableDuringDevelop: true, // Optional. Disables Crisp Chat during gatsby develop. Defaults to true.
+        defer: true, // Optional. Sets the Crisp loading script to defer instead of async. Defaults to false.
+        enableImprovedAccessibility: false, // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true.
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
