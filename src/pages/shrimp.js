64abd3d14110b2dd8f1461d8ef8087d3a0a44eee
Valdemar <voldemar136@gmail.com>
2020-07-21 21:00:51 +0300

    update shrimp

--- src/pages/shrimp.js
+++ src/pages/shrimp.js
@@ -9,7 +9,15 @@ function shrimp() {
     <Layout>
       <MenuFish />
       <div className="shrimp">
-        <div></div>
+        <div>
+          <h4>Креветка северная в/м 70/90 (Россия/Сахалин)</h4>
+          <p>
+            <span>Цена за 1 кг. в бел.руб. без НДС: </span>
+            <span>3,43</span>
+          </p>
+          <h5>НДС 10%</h5>
+          <h5>Фасовка: на паллетах, коробки или пакеты</h5>
+        </div>
       </div>
     </Layout>
   )
