f6444549786a7e82cb878caee7b49349208e5a40
Valdemar <voldemar136@gmail.com>
2020-09-23 16:27:00 +0300

    update price fish styles

--- src/components/MenuFish/MenuFish.js
+++ src/components/MenuFish/MenuFish.js
@@ -5,25 +5,28 @@ import "./MenuFish.scss"
 
 function MenuFish() {
   return (
-    <div className="menufish">
+    <div className="menuf">
       <a
+        className="price"
         href="https://drive.google.com/file/d/1HoCJ2APbw52NDmDCXjHz2Q_DaWmqw_1g/view?usp=sharing"
         target="_blank"
         rel="noreferrer"
       >
         ПРАЙС-ЛИСТ
       </a>
-      <Link to="/gorbusha">Горбуша</Link>
-      <Link to="/pollock">Минтай</Link>
-      <Link to="/herring">Сельдь</Link>
-      <Link to="/anchovy">Хамса</Link>
-      <Link to="/mackerel">Скумбрия</Link>
-      <Link to="/sprat">Килька</Link>
-      <Link to="/capelin">Мойва</Link>
-      <Link to="/cod">Треска</Link>
-      <Link to="/whiting">Путассу</Link>
-      <Link to="/squid">Кальмар</Link>
-      <Link to="/shrimp">Креветки</Link>
+      <div className="menufish">
+        <Link to="/gorbusha">Горбуша</Link>
+        <Link to="/pollock">Минтай</Link>
+        <Link to="/herring">Сельдь</Link>
+        <Link to="/anchovy">Хамса</Link>
+        <Link to="/mackerel">Скумбрия</Link>
+        <Link to="/sprat">Килька</Link>
+        <Link to="/capelin">Мойва</Link>
+        <Link to="/cod">Треска</Link>
+        <Link to="/whiting">Путассу</Link>
+        <Link to="/squid">Кальмар</Link>
+        <Link to="/shrimp">Креветки</Link>
+      </div>
     </div>
   )
 }
