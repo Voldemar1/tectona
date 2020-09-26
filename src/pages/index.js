f221b3a57d8c5eef08544523b5806c1fb336c848
Valdemar <voldemar136@gmail.com>
2020-09-26 17:42:42 +0300

    add feedback form for index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo"
 import Menu from "../components/Menu/Menu"
+import ContactForm from "../components/ContactForm/ContactForm"
 import { setConfig } from "react-hot-loader"
 
 import "./index.scss"
@@ -17,6 +18,7 @@ const IndexPage = () => (
       <SEO title="Home" />
       <h2>ГРУППА КОМПАНИЙ "ТЕКТОНА"</h2>
       <Menu />
+      <ContactForm />
     </Layout>
   </div>
 )
